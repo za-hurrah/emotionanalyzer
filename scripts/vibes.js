7bb23555b44d9873717e06cb927b64024b0562e7
Lez2 <leslie.nutkomlah@gmail.com>
2020-07-23 17:42:51 -0400

    functional analyzer

--- scripts/vibes.js
+++ scripts/vibes.js
@@ -1,36 +1,58 @@
-//const AFINN = require('./AFINN.json');
+let test = "I am miserable and depressed!!";
+let sum = 0;
+let submit = document.querySelector('#submit');
+let clear = document.querySelector('#clear');
 
-let test = "I am happy!!";
+
+//add event listener to begin sentiment analysis
+submit.addEventListener('click', start);
+
+//event listener that accesses what is typed in box
+clear.addEventListener('click', function(){
+  document.getElementById('text').value = "";
+});
+
+
+function start () {
+let input = document.getElementById('text').value;
 
 //makes all the text lowercase and splits words with spaces
 function tokenize(text) {
-  return text
+  return input
     .toLowerCase()
     .split(" ");
 }
-console.log(tokenize(test));
 
+//removes any characters in a word that we don't need in order to make comparing easier
 function deleteUselessChars(word){
-return word.replace(/[^\w]/g,"");
+  return word.replace(/[^\w]/g,"");
 }
-console.log(deleteUselessChars("word!!$#%^#$"));
+//pulls value from AFINN and updates sum of feeling
+function rateWord(word) {
+   for (const wvalue in AFINN) {
 
-let result = AFINN.filter(function(rate)){
-  console.log(rate);
+   if (word == wvalue) {
+      sum = sum + parseInt(AFINN[wvalue]);
+   }
+  }
+    return sum;
+}
+//analyzes a string of text
+function analyze(text) {
+    return tokenize(text)
+        .map(deleteUselessChars)
+        .map(rateWord);
+
+}
+//provides alert
+if (sum < 0){
+   alert("Your score is " + sum + "\n Wow. what a bummer you are.");
+}
+if(sum > 0){
+  alert("Your score is " + sum + "\n It seems you are quite positive. Chill.");
+}
+if (sum == 0){
+  alert("Your score is " + sum + "\nYou should try and add some more emotion to your text. Robots can do better then this");
 }
 
-// function rateWord(word) {
-//   return (word in "AFINN.json") ? AFINN [word] : 0;
-// }
-// console.log(rateWord("great"));
-
-// function sum(x, y) {
-//   return x + y;
-// }
-
-// function analyze(text) {
-//   return tokenize(text)
-//         .map(deleteUselessChars)
-//         .map(rateWord)
-//         .reduce(sum);
-// }
+}
